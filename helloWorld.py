4cbef143629709c3028a1fff151ea1d0d462883b
MikeAnz22 <tooseksy@gmail.com>
2023-03-26 20:53:59 -0400

    good

--- helloWorld.py
+++ helloWorld.py
@@ -2,3 +2,9 @@ print ("Hello World")
 print ("Hello World, my name is Mike and I love Python!")
 userName = input ("Hello! Please enter you name!")
 print ("Hello", userName, "nice to meet you")
+userName = input("Hello! Please enter your facebook username! ")
+userPassword = input("Hello! Please enter your facebook password! ")
+if(userName.lower() == "mike" and userPassword == "Juno123"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
